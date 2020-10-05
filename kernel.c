afbbb80c07c94e36f025bf8cc4045613f7f8ac8a
Carl Söyseth <carlsoyseth@gmail.com>
2020-10-06 00:47:05 +0200

    Now prints memory map from multi boot header

--- kernel.c
+++ kernel.c
@@ -1,10 +1,11 @@
+#include "multiboot.h"
+
 #include "mem.h"
 #include "tty.h"
 #include "int.h"
 #include "serial.h"
 
 
-//#include "multiboot.h"
 
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
@@ -16,19 +17,60 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
  
-
-extern uint32_t * freeList;
-
-
-void kernel_main(uint32_t* multiboot) 
+void kernel_main(unsigned long magic, unsigned long addr) 
 {
+	multiboot_info_t *mbi;
+
 	initSerial();
 	initTerminal();
- 	initGdt();
+	initGdt();
 	initIdt();
-	initPaging();  
 
- 	for(;;) {
-    	asm("hlt");
- 	}
+	if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
+	{
+		printf("MAGIC IS NOT MAGIC\n");
+		return;
+	}
+
+	mbi = (multiboot_info_t *) addr;
+
+	/* Print out the flags. */
+	printf ("flags = 0x%x\n", (unsigned) mbi->flags);
+
+
+	if (mbi->flags & MULTIBOOT_INFO_MEMORY) 
+	{
+		printf ("mem_lower = %uKB, mem_upper = %uKB\n",
+				(unsigned) mbi->mem_lower, (unsigned) mbi->mem_upper);
+ 
+	}
+
+	if (mbi->flags & MULTIBOOT_INFO_MEM_MAP)
+	{
+	  multiboot_memory_map_t *mmap;
+	  
+	  printf ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
+			  (unsigned) mbi->mmap_addr, (unsigned) mbi->mmap_length);
+
+	  for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr; (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
+		   mmap = (multiboot_memory_map_t *) ((unsigned long) mmap + mmap->size + sizeof (mmap->size)))
+		{
+			printf (" size = 0x%x, base_addr = 0x%x,"
+					" length = 0x%x, type = 0x%x\n",
+					(unsigned) mmap->size,
+					(unsigned) (mmap->addr >> 32),
+					(unsigned) (mmap->addr & 0xffffffff),
+					(unsigned) (mmap->len >> 32),
+					(unsigned) (mmap->len & 0xffffffff),
+					(unsigned) mmap->type);
+		}
+	}
+
+	initPaging();  
+ 
+
+
+	for(;;) {
+		asm("hlt");
+	}
 }
